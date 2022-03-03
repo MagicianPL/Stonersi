94614e7b999596a292fe7d3c00dfedd60d192337
Michał <sukowskimichal@gmail.com>
2022-03-03 17:03:55 +0100

    StyledComponents dependency

---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TopBar = () => {
+    return(
+        <div>
+            <ul>
+                <li>Zaloguj</li>
+                <li>Rejestracja</li>
+            </ul>
+        </div>
+    )
+};
+
+export default TopBar;
