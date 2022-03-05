bdb0ae19f07c93f4f47ba0e224c508230d2b02b3
Michał <sukowskimichal@gmail.com>
2022-03-05 11:12:15 +0100

    MainNav cmp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import CannabisImage from '../../images/cannabis.png';
+import MainNav from '../MainNav/MainNav';
 
 const StyledHeader = styled.header`
     width: 100%;
-    height: 240px;
+    height: 270px;
+    padding: 15px 8px;
     position: relative;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 
@@ -15,13 +18,18 @@ const StyledHeader = styled.header`
         position: absolute;
         inset: 0;
         background-image: url(${CannabisImage});
+        background-size: cover;
         opacity: 0.25;
+        z-index: -1;
     }
 
     h1 {
         font-family: SmokingTequila;
         color: #346137;
         font-size: 112px;
+        position: relative;
+        top: 20px;
+        cursor: pointer;
     }
 `;
 
@@ -29,6 +37,7 @@ const Header = () => {
     return(
         <StyledHeader>
             <h1>STONERSI</h1>
+            <MainNav />
         </StyledHeader>
     );
 };
