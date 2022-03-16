f3302db9485664d287c0d2b1a2299f8c8b1ed02e
Michał <sukowskimichal@gmail.com>
2022-03-16 19:49:27 +0100

    Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,76 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import StyledPost from './StyledPost';
 import { FaJoint } from 'react-icons/fa';
-
-const StyledPost = styled.div`
-    display: flex;
-    flex-direction: column;
-    border-radius: 4px;
-    overflow: hidden;
-    padding: 5px 10px 15px 10px;
-    background: rgba(152,186,79, 0.1);
-    margin-bottom: 35px;
-
-    & > div {
-        display: flex;
-        justify-content: space-around;
-    }
-
-    .top {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-
-        p:nth-child(1) {
-            cursor: pointer;
-            font-weight: bold;
-            transition: all 0.3s;
-
-            &:hover {
-                text-shadow: 0px 3px 2px rgba(0,0,0,0.67);
-            }
-        }
-    }
-
-    .joints {
-        margin-left: 36px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        position: relative;
-        padding-right: 5px;
-
-        &::before {
-            content: "";
-            position: absolute;
-            top: 0;
-            right: 0;
-            transform: translate(50%, -50%);
-            width: 100px;
-            height: 100px;
-            border-radius: 50%;
-            background: rgba(74, 161, 80, 0.4);
-            z-index: -1;
-            
-        }
-
-        span {
-            font-size: 14px;
-        }
-
-        span:nth-child(2) {
-            font-weight: bold;
-            font-size: 18px;
-        }
-    }
-
-    & > p {
-        text-align: center;
-        font-size: 20px;
-        max-width: 80%;
-        margin: 20px auto 30px auto;
-    }
-`;
 
 interface IProps {
     joints: number
@@ -88,7 +18,7 @@ const Post: React.FC<IProps> = ({joints, post, comments, author}) => {
             </div>
             <p>{post}</p>
             <div>
-                <p>Komentarze({comments.length})</p>
+                <p className="comments">Komentarze({comments.length})</p>
                 <p>~ {author}</p>
             </div>
         </StyledPost>
---
+++ src/components/Post/StyledPost.tsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+const StyledPost = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-radius: 4px;
+    overflow: hidden;
+    padding: 5px 10px 15px 10px;
+    background: rgba(152,186,79, 0.1);
+    margin-bottom: 35px;
+
+    & > div {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .top {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+
+        p:nth-child(1) {
+            cursor: pointer;
+            font-weight: bold;
+            transition: all 0.3s;
+
+            &:hover {
+                text-shadow: 0px 3px 2px rgba(0,0,0,0.67);
+            }
+        }
+    }
+
+    .joints {
+        margin-left: 36px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        padding-right: 5px;
+
+        &::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            right: 0;
+            transform: translate(50%, -50%);
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            background: rgba(74, 161, 80, 0.4);
+            z-index: -1;
+            
+        }
+
+        span {
+            font-size: 14px;
+        }
+
+        span:nth-child(2) {
+            font-weight: bold;
+            font-size: 18px;
+        }
+    }
+
+    & > p {
+        text-align: center;
+        font-size: 20px;
+        max-width: 80%;
+        margin: 20px auto 30px auto;
+    }
+
+    .comments {
+        cursor: pointer;
+    }
+`;
+
+export default StyledPost;
