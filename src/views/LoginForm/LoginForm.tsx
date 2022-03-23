18428febf0f2f275ea9a79c7fafff965edf67a15
Michał <sukowskimichal@gmail.com>
2022-03-23 21:48:55 +0100

    working on Modal funcionality on LoginForm

--- src/views/LoginForm/LoginForm.tsx
+++ src/views/LoginForm/LoginForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import StyledForm from './StyledForm';
 import Input from '../../components/Input/Input';
 import StyledButton from '../../components/StyledButton/StyledButton';
-
+import Modal from '../../components/Modal/Modal';
 
 
 const LoginForm = () => {
@@ -20,17 +20,34 @@ const LoginForm = () => {
         });
     };
 
+    /**** MODAL  ****/
+    //Props for Modal
+    //If modalContent is falsy - modal is hidden
+    //If errorMessage is not falsy - modal is red
+    const [showModal, setShowModal] = useState(false);
+    const [modalErrorMessage, setModalErrorMessage] = useState("");
+
+    const hideModal = () => {
+        setShowModal(false);
+        setModalErrorMessage("");
+    }
+    /****************************/
+
     const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(inputValues);
+        setShowModal(true);
     }
     return(
+        <>
         <StyledForm onSubmit={handleFormSubmit}>
             <h1>Zaloguj się</h1>
             <Input id="email" label="Adres e-mail" type="email" name="email" value={inputValues.email} onChange={handleInputChange} />
             <Input id="password" label="Twoje hasło" type="password" name="password" value={inputValues.password} onChange={handleInputChange} />
             <StyledButton center>Zaloguj</StyledButton>
         </StyledForm>
+        <Modal content="" showModal={showModal} hideModal={hideModal} />
+        </>
     );
 };
 
