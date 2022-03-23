8da24c62ef2cd6eb52410eb14575b88edf1622ea
Michał <sukowskimichal@gmail.com>
2022-03-23 21:34:41 +0100

    handling InputChange on LoginForm

--- src/views/LoginForm/LoginForm.tsx
+++ src/views/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StyledForm from './StyledForm';
 import Input from '../../components/Input/Input';
 import StyledButton from '../../components/StyledButton/StyledButton';
@@ -6,11 +6,29 @@ import StyledButton from '../../components/StyledButton/StyledButton';
 
 
 const LoginForm = () => {
+    //Input values
+    const initialValues = {
+        email: "",
+        password: ""
+    };
+    const [inputValues, setInputValues] = useState(initialValues);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValues({
+            ...inputValues,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(inputValues);
+    }
     return(
-        <StyledForm>
+        <StyledForm onSubmit={handleFormSubmit}>
             <h1>Zaloguj się</h1>
-            <Input id="email" label="Adres e-mail" type="email" />
-            <Input id="password" label="Twoje hasło" type="password" />
+            <Input id="email" label="Adres e-mail" type="email" name="email" value={inputValues.email} onChange={handleInputChange} />
+            <Input id="password" label="Twoje hasło" type="password" name="password" value={inputValues.password} onChange={handleInputChange} />
             <StyledButton center>Zaloguj</StyledButton>
         </StyledForm>
     );
