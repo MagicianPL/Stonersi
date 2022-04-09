6eff4a040659ef553ef807acb892c387fde0b491
Michał <sukowskimichal@gmail.com>
2022-04-09 17:40:30 +0200

    fetching post on Post Page

--- src/views/PostPage/PostPage.tsx
+++ src/views/PostPage/PostPage.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const PostPage = () => {
+
+    const { id: postId} = useParams();
+
+    const [fetchingError, setFetchingError] = useState("");
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+           const res = await fetch(`${process.env.REACT_APP_API}/posts/${postId}`);
+           const data = await res.json();
+
+           if(!res.ok) {
+            setFetchingError(data.message);
+           } else {
+               setPost(data);
+           };
+        };
+
+        fetchPost();
+    }, [postId]);
+
+    useEffect(() => {
+        console.log(post)
+    })
     return(
         <h1>Post Page</h1>
     );
