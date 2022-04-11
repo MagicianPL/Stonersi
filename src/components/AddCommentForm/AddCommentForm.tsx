855f35737b68353e51941bfbf2a481cd4ab1724c
Michał <sukowskimichal@gmail.com>
2022-04-11 14:33:44 +0200

    AddCommentForm cmp in build

---
+++ src/components/AddCommentForm/AddCommentForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.2);
+    position: fixed;
+    top: 0;
+    left: 0;
+`;
+
+const AddCommentForm = () => {
+    return(
+        <StyledWrapper>
+
+        </StyledWrapper>
+    );
+};
+
+export default AddCommentForm;
