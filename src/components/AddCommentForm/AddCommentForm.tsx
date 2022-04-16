323bb2b18d9f45ffebbbd20803cb15b7e66881a9
Michał <sukowskimichal@gmail.com>
2022-04-16 15:24:06 +0200

    architecture

--- src/components/AddCommentForm/AddCommentForm.tsx
+++ src/components/AddCommentForm/AddCommentForm.tsx
@@ -1,65 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
+import StyledWrapper from './StyledWrapper';
 import Input from '../Input/Input';
 import StyledButton from '../StyledButton/StyledButton';
 import { TailSpin } from  'react-loader-spinner'
 import { hideCommentModal, createNewComment } from '../../state/actions/commentsActions';
 
-const StyledWrapper = styled.div<{loading: Boolean, success: any, error: any}>`
-    width: 100%;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.5);
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 10;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-top: 30px;
-
-    form {
-        background: white;
-        border-radius: 8px;
-        padding: 15px 10px;
-        width: 100%;
-        max-width: 800px;
-        min-height: 100px;
-        ${ (({loading, success, error}) => loading || success || error ? `
-        display: flex;
-        justify-content: center;
-        ` : null)
-
-        }
-
-        h1 {
-            text-align: center;
-            margin-bottom: 25px;
-            background: rgba(0, 0, 0, 0.1);
-            padding: 5px 0;
-            border-radius: 4px;
-        }
-
-        textarea {
-            min-height: 100px;
-            font-size: 18px;
-        }
-
-        .actions {
-            width: 100%;
-            display: flex;
-            gap: 20px;
-            justify-content: flex-start;
-        }
-
-        p {
-            text-align: center;
-            font-weight: bold;
-            font-size: 24px;
-        }
-    }
-`;
 
 interface IProps {
     postId: String
