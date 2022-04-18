9e7f65f528f7997049fd9b01a7e4b8bcd65aaff6
Michał <sukowskimichal@gmail.com>
2022-04-18 19:50:36 +0200

    Link to homepage

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import StyledWrapper from './StyledWrapper';
+import { Link } from 'react-router-dom';
 
 const MainNav = () => {
     return (
             <StyledWrapper>
-                <li>Główna</li>
+                <Link to="/"><li>Główna</li></Link>
                 <li>Top 5</li>
                 <li>Filmy</li>
                 <li>FAQ</li>
