8f7538442dd4b54f002c0c5cf7ea72cdf9227387
Michał <sukowskimichal@gmail.com>
2022-04-28 23:14:56 +0200

    faq on separately file

---
+++ src/faq.tsx
@@ -0,0 +1,28 @@
+const faq = [
+    {
+        q: "Czemu służy portal Stonersi?",
+        a: "Strona powstała w celu edukacyjno - chilloutowo - humorystycznym :)"
+    },
+    {
+        q: "Dlaczego mojego wpisu nie widać na stronie głównej?",
+        a: "Wszystkie dodane przez użytkowników wpisy muszą zostać zatwierdzone przez moderatora."
+    },
+    {
+        q: "O co chodzi z tymi 'jointami do przypalenia?'",
+        a: "Każdy dodany post zbiera punkty, tzw. przypalone jointy. Zalogowany użytkownik ma do wykorzystania 3 jointy do przypalenia każdego dnia, które może przyznać dowolnym wpisom."
+    },
+    {
+        q: "Pozostało mi 0 jointów do przypalenia, co teraz?",
+        a: "Ilość jointów jest resetowana każdego dnia. Jeśli kolejnego dnia po zalogowaniu nadal widzisz 0, poczekaj chwilę, niedługo powinieneś otrzymać swoje 'punkty do przyznania' ;)"
+    },
+    {
+        q: "W zakładce FILMY nie mogę obejrzeć niektórych filmów",
+        a: "Wynika to z polityki serwisu Youtube. Czasami filmy są objęte ograniczeniami wiekowymi i można je obejrzeć tylko w wyżej wymienionym serwisie."
+    },
+    {
+        q: "Co to za ikona przekreślonego głośnika u góry?",
+        a: "Kliknij, może nieco podgłośnij swoje głośniki i poczekaj chwilę ;)"
+    }
+];
+
+export default faq;
