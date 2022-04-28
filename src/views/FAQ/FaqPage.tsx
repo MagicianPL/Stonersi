319c732b2d4a2a137adb40a78a2e826302ba4500
Michał <sukowskimichal@gmail.com>
2022-04-28 21:27:58 +0200

    FaqPage in build

---
+++ src/views/FAQ/FaqPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StyledContainer from './StyledContainer';
+
+
+const FaqPage = () => {
+    return(
+        <StyledContainer>
+            <h1>FAQ</h1>
+        </StyledContainer>
+    );
+};
+
+export default FaqPage;
---
+++ src/views/FAQ/StyledContainer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+    h1 {
+        color: #224024;
+        text-align: center;
+        padding: 20px 0;
+    }
+`;
+
+export default StyledContainer;
