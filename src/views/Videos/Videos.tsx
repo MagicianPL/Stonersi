9b6d7e5cf945b1327027262450f2eb0158282ab5
Michał <sukowskimichal@gmail.com>
2022-04-28 19:21:33 +0200

    Mocked data deleted

--- src/views/Videos/Videos.tsx
+++ src/views/Videos/Videos.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import StyledContainer from './StyledContainer';
 import Movie from '../../components/Movie/Movie';
 import { Rings } from 'react-loader-spinner';
-import movies from './movies';
-
 
 
 const Videos = () => {
-
     return(
         <StyledContainer>
             <h1>FILMY</h1>
