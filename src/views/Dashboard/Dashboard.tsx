f3891a4f6f3413a133c860dcdd2270ca12f519f2
Michał <sukowskimichal@gmail.com>
2022-05-01 00:03:44 +0200

    Conditionally rendering on content div

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -6,6 +6,8 @@ import PostsView from './SubPages/Posts/PostsView';
 const Dashboard = () => {
     const { user } = useSelector((state: any) => state.userReducer);
 
+    const [renderViewFromMenu, setRenderViewFromMenu] = useState<null | string>(null);
+
     //Posts
     const [allPosts, setAllPosts] = useState([]);
     useEffect(() => {
@@ -13,20 +15,24 @@ const Dashboard = () => {
         .then(res => res.json())
         .then(data => setAllPosts(data.posts.reverse()));
     }, []);
-    console.log(allPosts);
+
+    //For displaying right view (component) on content div when user clicks on menu
+    const renderComponent = (name: string) => {
+        setRenderViewFromMenu(name)
+    };
 
     return(
         <StyledWrapper>
             <nav>
                 <ul>
-                    <li>Posty</li>
+                    <li onClick={() => renderComponent("posts")}>Posty</li>
                     <li>Option 2</li>
                     <li>Option 3</li>
                 </ul>
             </nav>
             <div className="content">
                 <p className="greeting"><span>Co dziś działamy, {user?.login}?</span></p>
-                <PostsView posts={allPosts} />
+                {renderViewFromMenu === "posts" && <PostsView posts={allPosts} />}
             </div>
         </StyledWrapper>
     );
